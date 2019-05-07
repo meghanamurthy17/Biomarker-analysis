9d843f451ac35d6b3db588088b1467a950c01332
Meghana Murthy <33783644+meghanamurthy17@users.noreply.github.com>
2019-05-07 22:54:15 +0530

    Add files via upload

---
+++ public/resources/js/app.js
@@ -0,0 +1,58 @@
+
+var app = angular.module('products', []);
+
+app.controller('productCTRL', function ($scope, $http) {
+    
+    $scope.loader = {
+        loading: false
+    };
+	
+	$scope.predict = function () {
+		
+		$scope.loader.loading = true;
+        
+        $http.put('/predict', {
+            'input1' : $scope.input1,
+			'input2' : $scope.input2,
+			'input3' : $scope.input3,
+			'input4' : $scope.input4,
+			'input5' : $scope.input5,
+			'input6' : $scope.input6
+        })
+            .success(function (data, status, headers, config) {
+                console.log("input is passed in put statement");
+            })
+            .error(function (data, status, headers, config) {
+                $scope.loader.loading = false;
+                $scope.modalstatustext = "Unable to pass input!";
+            });
+		
+	};
+
+    //Read all entries
+    $scope.getAllreg = function () {
+        
+        $scope.loader.loading = true;
+        
+        $http.get("biomarkers")
+            .success(function (response) {
+                if (response.error === 2) {
+					//if error code is returned from node code, then there are no entries in db!
+					$scope.statustext = "There are currently no added accounts.";
+					$scope.loader.loading = false;
+				} else {
+					$scope.user=response.user;
+					$scope.names = response.owner;
+					$scope.forsale=response.status;
+					//Turn off spinner
+					$scope.loader.loading = false;
+					$scope.statustext = "";
+				}
+            })
+            .error(function (data, status, headers, config) {
+                $scope.loader.loading = false;
+                $scope.statustext = "There was an error fetching data, please check database connection.";
+            });
+    };
+
+});
---
+++ server.js
@@ -0,0 +1,89 @@
+var express = require('express');
+var app = express();
+var bodyParser = require("body-parser");
+var id;
+ var ObjectID = require('mongodb').ObjectID;
+//STATIC FILES
+app.use(express.static('public'));
+app.use(bodyParser.json()); // Body parser use JSON data
+
+var MongoClient = require('mongodb').MongoClient;
+  
+  var json1={};
+    var user;
+  
+    app.get('/', function (req, res) {
+	res.sendFile( __dirname + "/public" +"/"+ "index1.html" );
+});
+  
+  app.get('/input', function(req, res) {
+  	res.sendFile(__dirname + "/public" +"/"+"predict-index.html");
+	  //res.render('predict-disp.ejs');  
+  });
+  
+  app.put('/predict', function (req, res) {
+	  
+	var input1 = req.body.input1;
+	  var input2 = req.body.input2;
+	  var input3 = req.body.input3;
+	  var input4 = req.body.input4;
+	  var input5 = req.body.input5;
+	  var input6 = req.body.input6;
+    const { spawn } = require('child_process');
+    const pyProg = spawn('python', ['C:/Users/Meghana/Desktop/RFmodelPredict.py', input1, input2, input3, input4, input5, input6]);
+
+    pyProg.stdout.on('data', function(data) {
+		console.log("in hello.py");
+        console.log(data.toString());
+    });
+	
+        res.render('predict-disp.ejs');  
+})
+
+  
+// Connect to the db
+MongoClient.connect('mongodb://localhost', function (err, client) {
+  if (err) throw err;
+
+  var db = client.db('prop');
+ 
+  app.get('/biomarkers', function (req, res) {
+	console.log("GET Request :: /list");
+	  console.log(user);
+	 
+	var data = {
+        "error": 1,
+        "products": ""
+    };
+	
+  db.collection('property', function (err, collection) {
+        
+         collection.find().toArray(function(err, rows) {
+            if(err) throw err;    
+			if (rows.length !== 0 && !err) {
+				json1["error"] = 0;
+				data["products"] = rows;
+				json1["user"]=user;
+				json1['owner']=rows;
+							res.json(json1);
+				
+			} else if (rows.length === 0) {
+				data["error"] = 2;
+				data["products"] = 'No accounts Found..';
+				res.json(data);
+			} else {
+				data["products"] = 'error while performing query';
+				res.json(data);
+				console.log('Error while performing Query: ' + err);
+			}
+        });
+	  console.log(json1);
+	  });
+  });
+
+var server = app.listen(8081, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log("dummy app listening at: " + host + ":" + port);
+});
+});
